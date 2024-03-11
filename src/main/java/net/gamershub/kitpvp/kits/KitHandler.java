2be9c3f4c3d7b950e411988708e4505b895a13db
David <chaosdave34@users.noreply.github.com>
2024-03-11 17:04:17 +0100

    Removed KitHandler#clearKit

--- src/main/java/net/gamershub/kitpvp/kits/KitHandler.java
+++ src/main/java/net/gamershub/kitpvp/kits/KitHandler.java
@@ -1,13 +1,8 @@
 package net.gamershub.kitpvp.kits;
 
 import lombok.Getter;
-import net.gamershub.kitpvp.ExtendedPlayer;
-import net.gamershub.kitpvp.KitPvpPlugin;
 import net.gamershub.kitpvp.Utils;
 import net.gamershub.kitpvp.kits.impl.*;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.attribute.AttributeInstance;
-import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -52,18 +47,5 @@ public class KitHandler {
         kits.put(kit.getId(), kit);
         Utils.registerEvents(kit);
         return kit;
-    }
-
-    public void clearKit(Player p) {
-        ExtendedPlayer extendedPlayer = KitPvpPlugin.INSTANCE.getExtendedPlayer(p);
-        extendedPlayer.setSelectedKitId(null);
-
-        AttributeInstance maxHealth = p.getAttribute(Attribute.GENERIC_MAX_HEALTH);
-        if (maxHealth != null) maxHealth.setBaseValue(20);
-
-        AttributeInstance movementSpeed = p.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED);
-        if (movementSpeed != null) movementSpeed.setBaseValue(0.1);
-
-        p.getInventory().clear();
     }
 }
