6f336a59c209547991d4b80c16041b3d81a88cbe
palulski <paul.scheiner.regensburg@gmail.com>
2024-05-20 22:52:55 +0200

    improved dash ult

--- src/main/kotlin/io/github/chaosdave34/kitpvp/ultimates/impl/DashUltimate.kt
+++ src/main/kotlin/io/github/chaosdave34/kitpvp/ultimates/impl/DashUltimate.kt
@@ -1,8 +1,15 @@
 package io.github.chaosdave34.kitpvp.ultimates.impl
 
+import io.github.chaosdave34.kitpvp.KitPvp
+import io.github.chaosdave34.kitpvp.abilities.AbilityRunnable
 import io.github.chaosdave34.kitpvp.ultimates.Ultimate
 import net.kyori.adventure.text.Component
+import org.bukkit.Material
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.Horse
+import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 
 class DashUltimate: Ultimate("dash", "Dash", 50.0) {
     override fun getDescription(): Component {
@@ -10,6 +17,63 @@ class DashUltimate: Ultimate("dash", "Dash", 50.0) {
     }
 
     override fun onAbility(player: Player) { // Todo: Damage and knock back
-        player.velocity = player.eyeLocation.direction.multiply(4)
+
+        val horse = player.location.world.spawn(player.location, Horse::class.java) { horse ->
+            horse.owner = player
+            horse.isInvulnerable = true
+        }
+        horse.addPassenger(player)
+        val direction = player.eyeLocation.direction.multiply(5)
+        horse.velocity = direction
+        val content = player.inventory.storageContents
+        player.inventory.storageContents = arrayOf()
+        player.inventory.addItem(ItemStack(Material.DIAMOND_SWORD))
+        object : AbilityRunnable(player){
+            override fun runInGame() {
+
+                for (entity in player.getNearbyEntities(3.0, 3.5, 3.0)) {
+                    if(entity is LivingEntity){
+                        entity.velocity = direction.multiply(2);
+                        entity.damage(7.5,player)
+                    }
+                }
+                if(i > 1.5 * 20){
+                    cancel()
+                }
+            }
+        }.runTaskTimer(KitPvp.INSTANCE,1,1)
+        AbilityRunnable.runTaskLater(KitPvp.INSTANCE,{horse.remove()},player, 20*20)
+        AbilityRunnable.runTaskLater(KitPvp.INSTANCE,{player.inventory.storageContents = content},player, 20*20)
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
