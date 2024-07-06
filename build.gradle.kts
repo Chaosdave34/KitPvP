e0c6b783cbe9590e6af19499b6576a5598b949ab
Chaosdave34 <david.emanuel@gmx.de>
2024-07-06 22:46:44 +0200

    Removed dependency GHUtils and cleaned up build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,6 @@ plugins {
     id("xyz.jpenilla.run-paper") version "2.3.0" // Adds runServer and runMojangMappedServer tasks for testing
     id("xyz.jpenilla.resource-factory-paper-convention") version "1.1.1" // Generates plugin.yml based on the Gradle config
 
-    // Shades and relocates dependencies into our plugin jar. See https://imperceptiblethoughts.com/shadow/introduction/
-    //id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("io.github.goooler.shadow") version "8.1.7" // fork for java 21 support
-
     id("io.freefair.lombok") version "8.6"
 }
 
@@ -38,22 +34,13 @@ tasks.assemble {
 }
  */
 
-repositories {
-    mavenLocal()
-}
-
 dependencies {
     paperweight.paperDevBundle("1.21-R0.1-SNAPSHOT")
     // paperweight.foliaDevBundle("1.21-R0.1-SNAPSHOT")
     // paperweight.devBundle("com.example.paperfork", "1.21-R0.1-SNAPSHOT")
-    implementation("io.github.chaosdave34", "ghutils", "0.6.0-SNAPSHOT")
 }
 
 tasks {
-    assemble {
-        dependsOn(shadowJar)
-    }
-
     compileJava {
         // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
         // See https://openjdk.java.net/jeps/247 for more information.
@@ -64,35 +51,14 @@ tasks {
         options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
     }
 
-    processResources {
-        filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
-        val props = mapOf(
-            "name" to project.name,
-            "version" to project.version,
-            "description" to project.description,
-            "apiVersion" to "1.21"
-        )
-        inputs.properties(props)
-        filesMatching("plugin.yml") {
-            expand(props)
-        }
+    // Only relevant when going with option 2 above
+    /*
+    reobfJar {
+      // This is an example of how you might change the output location for reobfJar. It's recommended not to do this
+      // for a variety of reasons, however it's asked frequently enough that an example of how to do it is included here.
+      outputJar = layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar")
     }
-
-        // Only relevant when going with option 2 above
-        /*
-        reobfJar {
-          // This is an example of how you might change the output location for reobfJar. It's recommended not to do this
-          // for a variety of reasons, however it's asked frequently enough that an example of how to do it is included here.
-          outputJar = layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar")
-        }
-         */
-
-    shadowJar {
-        // helper function to relocate a package into our package
-        fun relocate(pkg: String) = relocate(pkg, group + rootProject.name + "dependency.$pkg")
-
-        relocate("io.github.chaosdave34.ghutils")
-    }
+     */
 }
 
 // Configure plugin.yml generation
